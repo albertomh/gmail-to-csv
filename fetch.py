416defb860898ea04a671d206322e467ef71986a
Alberto Morón Hernández <albertomh@users.noreply.github.com>
2016-03-29 20:32:28 -0400

    Update fetch.py

--- fetch.py
+++ fetch.py
@@ -34,3 +34,32 @@ def get_credentials():
             credentials = tools.run(flow, store)
         print('Storing credentials to ' + credential_path)
     return credentials
+
+
+def getLabel():
+
+  credentials = getCredentials()
+  http = credentials.authorize(httplib2.Http())
+  service = discovery.build('gmail', 'v1', http=http)
+
+  query='label:411-web-exp'  
+  
+  try:
+    response = service.users().messages().list(userId="me", q=query).execute()
+    messages = []
+    if 'messages' in response:
+      messages.extend(response['messages'])
+
+    while 'nextPageToken' in response:
+      page_token = response['nextPageToken']
+      response = service.users().messages().list(userId=user_id, q=query, pageToken=page_token).execute()
+      messages.extend(response['messages'])
+
+    l_id = list()
+    for i in range(len(messages)):
+        l_id.append(messages[i]['id'])
+        
+    return l_id
+        
+  except errors.HttpError, error:
+    print 'An error occurred: %s' % error
